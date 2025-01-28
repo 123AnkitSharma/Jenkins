555bde5e8838baac6635426a48a5050ed498219d
Ankit <84094757+123AnkitSharma@users.noreply.github.com>
2025-01-28 15:28:08 +0530

    Update HelloWorld2.py

--- HelloWorld2.py
+++ HelloWorld2.py
@@ -2,3 +2,5 @@
 # This Phython program will print Hellow World...
 print("Hello World Demo......\n")
 print("Hello World Demo......\n")
+print("Hello World Demo......\n")
+print("Hello World Demo......\n")
